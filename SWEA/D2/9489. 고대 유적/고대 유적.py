12d2c36bcddfde226fc10340bbc4931c4227c6e8
Belluable <78305537+Belluable@users.noreply.github.com>
2022-08-10 16:18:06 +0900

    [D2] Title: 고대 유적, Time: 187 ms, Memory: 61,724 KB -BaekjoonHub

---
+++ SWEA/D2/9489. 고대 유적/고대 유적.py
@@ -0,0 +1,28 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    ans = 0
+
+    for i in range(n):  # 너비
+        cnt = 0
+        for j in range(m):
+            if arr[i][j] == 1:
+                cnt += 1
+                if cnt > ans:
+                    ans = cnt
+            else:
+                cnt = 0
+
+    for j in range(m):  # 깊이
+        cnt = 0
+        for i in range(n):
+            if arr[i][j] == 1:
+                cnt += 1
+                if cnt > ans:
+                    ans = cnt
+            else:
+                cnt = 0
+
+    print(f'#{tc} {ans}')
