8299e6fd7d147cc1f4c65b58cb45c4c3848eabab
Belluable <78305537+Belluable@users.noreply.github.com>
2022-12-09 12:57:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -2,9 +2,8 @@ def solution(n,a,b):
     answer = 0
     for _ in range(n//2):
         answer += 1
-        if (a+1)//2 == (b+1)//2:
+        a, b = (a+1)//2, (b+1)//2
+        if a == b:
             break
-        else:
-            a, b = (a+1)//2, (b+1)//2
-    
+            
     return answer
